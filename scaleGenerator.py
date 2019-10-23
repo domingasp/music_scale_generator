863961f999a0aa39b2188a5616c6a9b9ba3dd891
Domingas <domingas@Domingass-MacBook-Pro.local>
2019-10-23 08:40:32 +0100

    Finished the functionality to generate a scale (major and minor only).

--- scaleGenerator.py
+++ scaleGenerator.py
@@ -5,19 +5,31 @@ twoOctaves = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', '
 majorScale = [2, 2, 1, 2, 2, 2, 1]
 minorScale = [2, 1, 2, 2, 1, 2, 2]
 
+# Generates the selected scale
 def generateScale(rootNote, scale):
     createdScale = []
     indexOfFirstRootNote = 0
 
+    # Determines the location of the first note in the twoOctaves list
     counter = 0
     while counter < len(twoOctaves):
         if (rootNote == twoOctaves[counter]):
             indexOfFirstRootNote = counter
+            break
         counter += 1
-    print(indexOfFirstRootNote)
+    
+    # Generates the scale based on the steps in the chosen scale
+    for step in scale:
+        createdScale.append(twoOctaves[indexOfFirstRootNote])
+        indexOfFirstRootNote += step
+    createdScale.append(rootNote)
 
+    return createdScale
+
+# Runs the main application
 def main():
-    generateScale('C#', majorScale)
+    print(generateScale('C', minorScale))
 
+# Entry of the script is the main() function
 if __name__ == '__main__':
     main()
