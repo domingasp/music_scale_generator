54628b4caece1bab596e4843e7109427eed54928
Domingas <domingas@Domingass-MacBook-Pro.local>
2019-10-24 17:15:28 +0100

    Started to add GUI elements. Currently only added an OptionMenu for note selection, needs fixing.

--- scaleGenerator.py
+++ scaleGenerator.py
@@ -1,3 +1,6 @@
+# Import for GUI
+import tkinter as tk
+
 # Generates different musical scales
 twoOctaves = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
@@ -26,10 +29,27 @@ def generateScale(rootNote, scale):
 
     return createdScale
 
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Scale Generator")
+
+        self.noteSelection = tk.OptionMenu(self.master, "C", twoOctaves[0], twoOctaves[1], twoOctaves[2], twoOctaves[3], twoOctaves[4], twoOctaves[5], twoOctaves[6])
+        self.noteSelection.grid(row = 0, column = 0)
+
 # Runs the main application
 def main():
+    root = tk.Tk()
+    gui = GUI(root)
     print(generateScale('C', minorScale))
+
+    root.mainloop()
 
 # Entry of the script is the main() function
 if __name__ == '__main__':
     main()
+
+# TODO:
+# Make the script send a text to my telephone of the scale
+# Create a GUI which will make selecting a scale much simpler
+# Select a random scale. Easiest way would be to generate a random number from 0 - 6 as this will represent the first half of the twoOctaves list and cover all notes.
